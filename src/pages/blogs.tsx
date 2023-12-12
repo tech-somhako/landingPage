d2726e19e086284ce6593371d34bad4d4efef359
Doshi Naman <naman.programmer@gmail.com>
2023-12-12 10:40:20 +0530

    12/12/23

--- src/pages/blogs.tsx
+++ src/pages/blogs.tsx
@@ -12,7 +12,6 @@ import Link from "next/link";
 import ToggleLang from "@/components/noAuth/ToggleLang";
 import { useLangStore } from "@/utils/code";
 import BookADemo from "@/components/noAuth/BookADemo";
-import { axiosInstance2 } from "./api/axiosApi";
 
 export default function BlogsPage() {
 	const srcLang = useLangStore((state: { lang: any }) => state.lang);
@@ -42,35 +41,6 @@ export default function BlogsPage() {
 
 	const [state, setState] = useState([]);
 
-	const [publogs, setpublogs] = useState([]);
-	const [poblogs, setpoblogs] = useState([]);
-
-	async function loadProductUpdateBlogs() {
-		await axiosInstance2
-			.get("blogs/product-update/")
-			.then((response) => {
-				console.log("!!!!", "loadProductUpdateBlogs response", response.data);
-				setpublogs(response.data);
-			})
-			.catch((error) => {
-				setpublogs([]);
-				console.log("!!!!", "loadProductUpdateBlogs error", error);
-			});
-	}
-
-	async function loadProductOtherBlogs() {
-		await axiosInstance2
-			.get("blogs/other/")
-			.then((response) => {
-				console.log("!!!!", "loadProductOtherBlogs response", response.data);
-				setpoblogs(response.data);
-			})
-			.catch((error) => {
-				setpoblogs([]);
-				console.log("!!!!", "loadProductOtherBlogs error", error);
-			});
-	}
-
 	async function loadBlogs(mediumURL: any) {
 		await axiosInstance
 			.get(mediumURL)
@@ -79,22 +49,14 @@ export default function BlogsPage() {
 				setState(response.data.items);
 			})
 			.catch((error) => {
-				setState((prevState) => ({
-					...prevState,
-					error: error.toJSON()
-				}));
-				console.log(error);
+				setState([]);
+				console.log("!!!", "response err", error);
 			});
 	}
 
 	useEffect(() => {
 		const mediumURL = "https://api.rss2json.com/v1/api.json?rss_url=https://medium.com/feed/@Somhako";
 		loadBlogs(mediumURL);
-	}, []);
-
-	useEffect(() => {
-		loadProductUpdateBlogs();
-		loadProductOtherBlogs();
 	}, []);
 
 	useEffect(() => {
@@ -150,183 +112,163 @@ export default function BlogsPage() {
 					<div className="h-auto w-full  bg-[#F5F6F8]">
 						<div className="flex h-auto w-full flex-col items-center justify-start gap-2  p-8 max-lg:p-4 max-md:gap-10 max-md:p-2">
 							<div className="mt-[-16rem] flex h-auto w-[60vw] flex-row flex-wrap items-center justify-center gap-8 rounded  bg-white p-4  px-12 shadow-lg max-lg:h-auto max-lg:min-h-fit max-lg:w-[90vw] max-md:mt-[-8rem] max-md:flex-col max-md:p-2">
-								{publogs && publogs.length > 0 && (
-									<div className="h-auto w-full   p-8 max-lg:p-4 max-lg:pb-[4rem] max-md:p-2 max-md:pb-[4rem]">
-										<p className="w-full text-center text-3xl font-semibold tracking-[6.4px]">
-											{srcLang === "ja" ? "ソーシャルフォローで最新情報をチェック" : "Latest updates"}
-										</p>
+								{state && state.length > 0 && (
+									<div className="m-2 h-auto w-full   p-8 max-lg:p-4 max-lg:pb-[4rem] max-md:p-2 max-md:pb-[4rem]">
 										<Slider {...settings}>
-											{publogs.map((data, i) => (
-												<div key={i}>
-													<div
-														className="mx-auto flex w-full cursor-pointer flex-col items-center  bg-white p-3  md:max-w-3xl md:space-x-5 md:space-y-0 md:odd:flex-row md:even:flex-row-reverse"
-														// onClick={() => {
-														// 	window.open(data.link, "_blank");
-														// }}
-													>
-														<div className="h-auto w-[40%] max-md:w-full">
-															<Image
-																src={data.image.image}
-																alt={data.image.alt_text}
-																title={data.image.title}
-																width={500}
-																height={500}
-																className="ml-auto h-[20vh] w-full object-cover max-md:mx-auto max-md:h-full"
-															/>
-														</div>
+											{state &&
+												state.map((data, i) => (
+													<div key={i}>
+														<div
+															className="mx-auto flex w-full cursor-pointer flex-col items-center  bg-white p-3  md:max-w-3xl md:space-x-5 md:space-y-0 md:odd:flex-row md:even:flex-row-reverse"
+															onClick={() => {
+																window.open(data.link, "_blank");
+															}}
+														>
+															<div className="h-auto w-[40%] max-md:w-full">
+																<Image
+																	src={data.thumbnail}
+																	alt="why"
+																	width={500}
+																	height={500}
+																	className="ml-auto h-[20vh] w-full object-cover max-md:mx-auto max-md:h-full"
+																/>
+															</div>
 
-														<div className="flex h-full w-[60%] flex-col space-y-2 bg-white p-3 max-md:w-full">
-															<h3 className="w-[100%] truncate text-xl font-semibold text-gray-800 md:text-2xl">
-																{data.title}
-															</h3>
-															<Link href={"#"} target="_blank" className="text-[#2D129A] hover:underline">
-																read more
-															</Link>
+															<div className="flex h-full w-[60%] flex-col space-y-2 bg-white p-3 max-md:w-full">
+																<h3 className="w-[100%] truncate text-xl font-semibold text-gray-800 md:text-2xl">
+																	{data.title}
+																</h3>
+																<Link href={data.link} target="_blank" className="text-[#2D129A] hover:underline">
+																	read more
+																</Link>
+															</div>
 														</div>
 													</div>
-												</div>
-											))}
-											{/* {state &&
-											state.length > 0 &&
-											state.map((data, i) => (
-												<div key={i}>
-													<div
-														className="mx-auto flex w-full cursor-pointer flex-col items-center  bg-white p-3  md:max-w-3xl md:space-x-5 md:space-y-0 md:odd:flex-row md:even:flex-row-reverse"
-														onClick={() => {
-															window.open(data.link, "_blank");
-														}}
-													>
-														<div className="h-auto w-[40%] max-md:w-full">
-															<Image
-																src={data.thumbnail}
-																alt="why"
-																width={500}
-																height={500}
-																className="ml-auto h-[20vh] w-full object-cover max-md:mx-auto max-md:h-full"
-															/>
-														</div>
-
-														<div className="flex h-full w-[60%] flex-col space-y-2 bg-white p-3 max-md:w-full">
-															<h3 className="w-[100%] truncate text-xl font-semibold text-gray-800 md:text-2xl">
-																{data.title}
-															</h3>
-															<Link href={data.link} target="_blank" className="text-[#2D129A] hover:underline">
-																read more
-															</Link>
-														</div>
-													</div>
-												</div>
-											))} */}
+												))}
 										</Slider>
 									</div>
 								)}
 
-								<div>
-									<button
-										className="transform rounded-full bg-gradient-to-r from-blue-500 to-blue-700 px-3 py-1 text-[10px] tracking-wide text-white shadow-lg transition-all duration-500 ease-in-out hover:scale-110 hover:animate-pulse hover:from-blue-600 hover:to-blue-800 hover:brightness-110 active:animate-bounce"
-										// onClick={() => setbookADemo(true)}
-									>
-										Explore more blogs
-									</button>
+								<div className="flex w-full flex-col items-center justify-center gap-4 ">
+									<div className="flex items-center justify-center gap-4 text-lg text-black">
+										<Link
+											href="https://www.linkedin.com/company/somhakoo/"
+											target="_blank"
+											className="opacity-80 transition-opacity hover:opacity-100"
+										>
+											<i className="fa-brands fa-linkedin"></i>
+										</Link>
+										<Link
+											href="https://twitter.com/somhako"
+											target="_blank"
+											className="my-auto opacity-80 transition-opacity hover:opacity-100"
+										>
+											<svg
+												xmlns="http://www.w3.org/2000/svg"
+												height="1rem"
+												viewBox="0 0 512 512"
+												className="fill-black"
+											>
+												<path d="M389.2 48h70.6L305.6 224.2 487 464H345L233.7 318.6 106.5 464H35.8L200.7 275.5 26.8 48H172.4L272.9 180.9 389.2 48zM364.4 421.8h39.1L151.1 88h-42L364.4 421.8z" />
+											</svg>
+										</Link>
+										<Link
+											href="https://medium.com/@somhako"
+											target="_blank"
+											className="opacity-80 transition-opacity hover:opacity-100"
+										>
+											<i className="fa-brands fa-medium"></i>
+										</Link>
+										<Link
+											href="https://www.facebook.com/Somhako"
+											target="_blank"
+											className="opacity-80 transition-opacity hover:opacity-100"
+										>
+											<i className="fa-brands fa-facebook"></i>
+										</Link>
+									</div>
+									<p className="w-[80%] text-center text-3xl font-semibold tracking-[6.4px]">
+										{srcLang === "ja"
+											? "ソーシャルフォローで最新情報をチェック"
+											: "Follow us on socials to stay updated onthe latest"}
+									</p>
 								</div>
 
-								{poblogs.length > 0 || publogs.length > 0 ? (
-									<div className="flex w-full flex-col items-center justify-center gap-4 ">
-										<div className="flex items-center justify-center gap-4 text-lg text-black">
-											<Link
-												href="https://www.linkedin.com/company/somhakoo/"
-												target="_blank"
-												className="opacity-80 transition-opacity hover:opacity-100"
+								{state && state.length > 0 ? (
+									<>
+										{state.map((data, i) => (
+											<div
+												className="mx-auto flex w-full cursor-pointer flex-col items-center space-y-3 rounded-xl bg-white p-3 shadow-lg hover:shadow-xl md:max-w-3xl md:space-x-5 md:space-y-0 md:odd:flex-row md:even:flex-row-reverse"
+												key={i}
+												onClick={() => {
+													window.open(data.link, "_blank");
+												}}
 											>
-												<i className="fa-brands fa-linkedin"></i>
-											</Link>
-											<Link
-												href="https://twitter.com/somhako"
-												target="_blank"
-												className="my-auto opacity-80 transition-opacity hover:opacity-100"
-											>
-												<svg
-													xmlns="http://www.w3.org/2000/svg"
-													height="1rem"
-													viewBox="0 0 512 512"
-													className="fill-black"
-												>
-													<path d="M389.2 48h70.6L305.6 224.2 487 464H345L233.7 318.6 106.5 464H35.8L200.7 275.5 26.8 48H172.4L272.9 180.9 389.2 48zM364.4 421.8h39.1L151.1 88h-42L364.4 421.8z" />
-												</svg>
-											</Link>
-											<Link
-												href="https://medium.com/@somhako"
-												target="_blank"
-												className="opacity-80 transition-opacity hover:opacity-100"
-											>
-												<i className="fa-brands fa-medium"></i>
-											</Link>
-											<Link
-												href="https://www.facebook.com/Somhako"
-												target="_blank"
-												className="opacity-80 transition-opacity hover:opacity-100"
-											>
-												<i className="fa-brands fa-facebook"></i>
-											</Link>
-										</div>
-										<p className="w-[80%] text-center text-3xl font-semibold tracking-[6.4px]">
-											{srcLang === "ja"
-												? "ソーシャルフォローで最新情報をチェック"
-												: "Follow us on socials to stay updated onthe latest"}
-										</p>
-									</div>
-								) : (
-									<></>
-								)}
-
-								{poblogs &&
-									poblogs.length > 0 &&
-									poblogs.map((data, i) => (
-										<div
-											className="mx-auto flex w-full cursor-pointer flex-col items-center space-y-3 rounded-xl bg-white p-3 shadow-lg hover:shadow-xl md:max-w-3xl md:space-x-5 md:space-y-0 md:odd:flex-row md:even:flex-row-reverse"
-											key={i}
-											// onClick={() => {
-											// 	window.open(data.link, "_blank");
-											// }}
-										>
-											<div className="h-auto w-[40%] max-md:w-full">
-												<Image
-													src={data.image.image}
-													alt={data.image.alt_text}
-													title={data.image.title}
-													width={500}
-													height={500}
-													className="ml-auto h-[30vh] w-full object-cover max-md:mx-auto max-md:h-full"
-												/>
-											</div>
-
-											<div className="flex h-full w-[60%] flex-col space-y-2 bg-white p-3 max-md:w-full">
-												<div className="item-center flex justify-between">
-													<div className="hidden rounded-full bg-gray-200 px-3 py-1 text-xs font-medium text-gray-800 md:block">
-														{data.category.title}
-													</div>
+												<div className="h-auto w-[40%] max-md:w-full">
+													<Image
+														src={data.thumbnail}
+														alt="why"
+														width={500}
+														height={500}
+														className="ml-auto h-[30vh] w-full object-cover max-md:mx-auto max-md:h-full"
+													/>
 												</div>
-												<h3 className="w-[100%] truncate text-xl font-semibold text-gray-800 md:text-2xl">
-													{data.title}
-												</h3>
-												<Link href={"#"} target="_blank" className="text-[#2D129A] hover:underline">
-													read more
-												</Link>
-											</div>
-										</div>
-									))}
 
-								{poblogs.length > 0 || publogs.length > 0 ? (
-									<div>
-										<button
-											className="transform rounded-full bg-gradient-to-r from-blue-500 to-blue-700 px-3 py-1 text-[10px] tracking-wide text-white shadow-lg transition-all duration-500 ease-in-out hover:scale-110 hover:animate-pulse hover:from-blue-600 hover:to-blue-800 hover:brightness-110 active:animate-bounce"
-											// onClick={() => setbookADemo(true)}
-										>
-											Explore more blogs
-										</button>
-									</div>
+												<div className="flex h-full w-[60%] flex-col space-y-2 bg-white p-3 max-md:w-full">
+													{/* <div className="item-center flex justify-between">
+													<p className="hidden font-medium text-gray-500 md:block">Vacations</p>
+													<div className="flex items-center">
+														<svg
+															xmlns="http://www.w3.org/2000/svg"
+															className="h-5 w-5 text-yellow-500"
+															viewBox="0 0 20 20"
+															fill="currentColor"
+														>
+															<path d="M9.049 2.927c.3-.921 1.603-.921 1.902 0l1.07 3.292a1 1 0 00.95.69h3.462c.969 0 1.371 1.24.588 1.81l-2.8 2.034a1 1 0 00-.364 1.118l1.07 3.292c.3.921-.755 1.688-1.54 1.118l-2.8-2.034a1 1 0 00-1.175 0l-2.8 2.034c-.784.57-1.838-.197-1.539-1.118l1.07-3.292a1 1 0 00-.364-1.118L2.98 8.72c-.783-.57-.38-1.81.588-1.81h3.461a1 1 0 00.951-.69l1.07-3.292z" />
+														</svg>
+														<p className="ml-1 text-sm font-bold text-gray-600">
+															4.96
+															<span className="font-normal text-gray-500">(76 reviews)</span>
+														</p>
+													</div>
+													<div className="">
+														<svg
+															xmlns="http://www.w3.org/2000/svg"
+															className="h-5 w-5 text-pink-500"
+															viewBox="0 0 20 20"
+															fill="currentColor"
+														>
+															<path
+																fill-rule="evenodd"
+																d="M3.172 5.172a4 4 0 015.656 0L10 6.343l1.172-1.171a4 4 0 115.656 5.656L10 17.657l-6.828-6.829a4 4 0 010-5.656z"
+																clip-rule="evenodd"
+															/>
+														</svg>
+													</div>
+													<div className="hidden rounded-full bg-gray-200 px-3 py-1 text-xs font-medium text-gray-800 md:block">
+														Superhost
+													</div>
+												</div> */}
+													<h3 className="w-[100%] truncate text-xl font-semibold text-gray-800 md:text-2xl">
+														{data.title}
+													</h3>
+													<Link href={data.link} target="_blank" className="text-[#2D129A] hover:underline">
+														read more
+													</Link>
+												</div>
+											</div>
+										))}
+									</>
 								) : (
-									<></>
+									<>
+										<div className="flex w-full flex-col items-center justify-center gap-4 ">
+											<p className="w-[80%] text-center text-xl font-bold">
+												{srcLang === "ja"
+													? "申し訳ありませんが、現在ブログは利用できません。最新の情報や洞察に期待して、また遊びに来てください。"
+													: "Sorry, there are currently no blogs available. Check back soon for the latest updates and insightful content."}
+											</p>
+										</div>
+									</>
 								)}
 							</div>
 						</div>
